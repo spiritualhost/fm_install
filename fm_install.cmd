d3cc5cc7b650ea755055321d53eff40c58ee4152
spiritualhost <burntfurniture@gmail.com>
2026-04-01 08:52:11 -0700

    Set remaining AI variables to desired values; output all AI variables; cleanup

--- fm_install.cmd
+++ fm_install.cmd
@@ -2,80 +2,60 @@
 
 :: EULA acceptance
 set AI_LICENSE_ACCEPTED=1
-echo %AI_LICENSE_ACCEPTED%
 
 :: Get system user name, which should match the AD name used to log into FileMaker
 set AI_USERNAME=%USERNAME%
-echo %AI_USERNAME%
 
 :: Set default language value
 set AI_LANGUAGE=English
-echo %AI_LANGUAGE%
 
 :: Install for all users
 set AI_APPLICATIONUSERS=AllUsers
-echo %AI_APPLICATIONUSERS%
 
 :: Prevent users from creating custom apps
 set AI_NONEWDATABASES=1
-echo %AI_NONEWDATABASES%
 
 :: Create a shortcut for FileMaker Pro onm the desktop and in the Quick Launch toolbar
-set AI_SHORTCUTS 
-
-
+set AI_SHORTCUTS=1
 
 :: Suppress the personalization box during installation
-set AI_SKIPDIALOG
-
+set AI_SKIPDIALOG=1
 
 :: Enable the software update dialog box
-set AI_DISABLEUPDATENOTIFY
-
+set AI_DISABLEUPDATENOTIFY=0
 
 :: Enable the new version notification dialog box
-set AI_DISABLEVERSIONNOTIFY
-
-
+set AI_DISABLEVERSIONNOTIFY=0
 
 :: Enable the option for plugins to be installed by files
-set AI_DISABLEPLUGINS
+set AI_DISABLEPLUGINS=0
 
+:: Show the Enable ODBC/JDBC command
+set AI_DISABLEXDBC=0
 
-
-:: Hide or show the Enable ODBC/JDBC command
-set AI_DISABLEXDBC
-
-
-
-
-:: Hide or show the configure for FileMaker WebDirect command
-set AI_DISABLEIWP
-
+:: Show the configure for FileMaker WebDirect command
+set AI_DISABLEIWP=0
 
 :: Disable or enable the FileMaker Pro development and deployment features
-set AI_DISABLEADVANCEDTOOLS
-
-
+set AI_DISABLEADVANCEDTOOLS=0
 
 :: Open a custom app when Filemaker Pro starts -- uncomment the below to set up this feature up
 :: set AI_LAUNCH_CUSTOMAPP=
 
-:: Enable or disable the warning for opening apps using an fmp URL
-set AI_WARN_FMP_URL
+:: Disable the warning for opening apps using an fmp URL
+set AI_WARN_FMP_URL=0
 
 :: Enable or disable communication with a FileMaker host using port 443 instead of 5003 (HTTPS tunneling)
-set AI_USE_HTTP_PROTOCOL_NETWORK
-
-
+set AI_USE_HTTP_PROTOCOL_NETWORK=0 
 
 :: Set the file cache size (if not specified, default is 128 MB)
 :: set AI_SET_FILE_CACHE=
 
 :: Enable or disable accessing external URLs when starting FileMaker Pro
-set AI_DISABLEEXTERNALURLS
+set AI_DISABLEEXTERNALURLS=0
 
-
+::List all existing variables beginning with AI and their values
+set AI
 
 :: Set up AssistedInstall.txt
 
